aa353bf6ba0df94cba1e98999a20544359226f4f
Texas R3X <texasr3xphone@gmail.com>
2025-04-03 20:29:55 -0600

    employees can be added

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
 import { FormControl } from '@angular/forms';
+import { Employee } from '../../interfaces/employee';
 
 @Component({
   selector: 'app-timesheet',
@@ -14,15 +15,31 @@ export class TimesheetComponent implements OnInit {
   departments!: Department[];
   department!: Department;
   employeeNameFC = new FormControl("");
+  employees: Employee[] = [];
+  employeeId: number = 0;
 
   constructor(
     private route: ActivatedRoute,
     private departmentsService: DepartmentsService,
-
   ) { }
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+        this.employeeId++;
+
+        this.employees.push({
+            id: this.employeeId.toString(),
+            departmentId: this.department?.id,
+            name: this.employeeNameFC.value,
+            payRate: Math.floor(Math.random() * 50) + 50,
+        });
+
+        this.employeeNameFC.setValue('');
+    }
   }
 }
