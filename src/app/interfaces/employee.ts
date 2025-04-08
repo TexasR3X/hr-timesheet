c0332c364cc913a7ec9b1ac8addf4677868f2dc5
Texas R3X <texasr3xphone@gmail.com>
2025-04-08 09:06:20 -0600

    Added rows for each employee

--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -1,6 +1,15 @@
 export interface Employee {
+    [key: string]: string | number | undefined;
+
     id: string;
     departmentId: string | undefined;
     name: string;
     payRate: number;
+    monday: number;
+    tuesday: number;
+    wednesday: number;
+    thursday: number;
+    friday: number;
+    saturday: number;
+    sunday: number;
 }
