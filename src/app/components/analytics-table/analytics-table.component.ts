12c70251b53c47588625b3f732ea86b05ce8954b
Texas R3X <texasr3xphone@gmail.com>
2025-04-08 10:30:34 -0600

    Total hours & total pay shown in <app-analytics-table>s

--- src/app/components/analytics-table/analytics-table.component.ts
+++ src/app/components/analytics-table/analytics-table.component.ts
@@ -73,4 +73,14 @@ export class AnalyticsTableComponent implements OnInit {
   ngOnInit(): void {
     this.employees = this.employeeData.filter(employee => employee.departmentId === this.departmentId);
   }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday
+      + employee.tuesday
+      + employee.wednesday
+      + employee.thursday
+      + employee.friday
+      + employee.saturday
+      + employee.sunday;
+  }
 }
