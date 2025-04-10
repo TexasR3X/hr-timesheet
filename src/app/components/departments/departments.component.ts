ba23a8e2ccfa164f05a8f51a01acb670da8141f3
Texas R3X <texasr3xphone@gmail.com>
2025-04-10 10:05:52 -0600

    Added $departments

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -10,7 +11,8 @@ import { Router } from '@angular/router';
   styleUrl: './departments.component.scss'
 })
 export class DepartmentsComponent implements OnInit {
-  departments!: Department[];
+  departments!: Department[]; // I could probably delete departments with Observable Way 2.
+  $departments!: Observable<Department[]>;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -18,9 +20,13 @@ export class DepartmentsComponent implements OnInit {
   ) { }
   
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe(departments => {
-      this.departments = departments;
-    });
+    ///// Observable Way 1 /////
+    // this.departmentsService.getDepartments().subscribe(departments => {
+    //   this.departments = departments;
+    // });
+
+    ///// Observable Way 2 /////
+    this.$departments = this.departmentsService.getDepartments();
   }
 
   goToDepartment(departmentId: string): void {
